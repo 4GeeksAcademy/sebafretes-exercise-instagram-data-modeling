7c44516624b89f86b64b7e6dada858789253b79e
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-03-26 19:55:00 +0000

    models done

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -9,8 +9,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
     firstname = Column(String(250), nullable=False)
@@ -19,13 +17,41 @@ class User(Base):
 
 class Post(Base):
     __tablename__ = 'post'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
     
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500), nullable=False)
+
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author = relationship(User)
+
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String(250), nullable=False)
+
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_from = relationship(User)
+
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to = relationship(User)
+
 
     def to_dict(self):
         return {}
